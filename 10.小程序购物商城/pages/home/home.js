c3430cb75a374cfe6b8515cc5e13bd4ac525b000
liubo <2281075105@qq.com>
2019-06-01 17:37:01 +0800

    商城首页完成

--- 10.小程序购物商城/pages/home/home.js
+++ 10.小程序购物商城/pages/home/home.js
@@ -8,26 +8,84 @@ Page({
    */
   data: {
     recommend:[],
-    banner:[]
+    banner:[],
+    currentType:"pop",
+    goods:{
+      pop: { page: 1, list: [] },
+      new: { page: 1, list: [] },
+      sell: { page: 1, list: [] }
+    }
   },
   titleClick(event){
     console.log(event);
+    const index = event.detail.index
+    let type = "pop"
+    switch(index){
+      case 0:
+        type="pop"
+        break;
+      case 1:
+        type = "new"
+        break;
+      case 2:
+        type = "sell"
+        break;
+    }
+
+    this.setData({
+      currentType:type
+    })
+
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    this._getBannerAndRecommend()
+    this._getHomeGoods()
+  },
+  _getBannerAndRecommend(){
+    // 1.轮播图
     request({
-      url:"/home/multidata"
-    }).then(res=>{
+      url: "/home/multidata"
+    }).then(res => {
       console.log(res);
       // 取出数据
       const banner = res.data.data.banner.list
       const recommend = res.data.data.recommend.list
       this.setData({
-        banner:banner,
-        recommend:recommend
+        banner: banner,
+        recommend: recommend
+      })
+    })
+  },
+
+  _getHomeGoods(){
+    this._getGoodsWithType("pop")
+    this._getGoodsWithType("new")
+    this._getGoodsWithType("sell")
+  },
+  _getGoodsWithType(type){
+
+    const page = this.data.goods[type].page
+    // 2.下面的商品
+    request({
+      url: "/home/data",
+      data: {
+        type: type,
+        page: page
+      }
+    }).then(res => {
+      console.log(res);
+      // 取出数据
+      const lists = res.data.data.list
+      const goods = this.data.goods
+      goods[type].list.push(...lists)
+      goods[type].page += 1
+      this.setData({
+        goods: goods
       })
     })
   }
+
 })
